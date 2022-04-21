d6f60cfc1391b6a71bb581c9ffe1cdfcb29d8387
Chaimaelaouini <103863934+Chaimaelaouini@users.noreply.github.com>
2022-04-21 02:19:15 +0000

    Add files via upload

---
+++ Livre.java
@@ -0,0 +1,17 @@
+package gesbib;
+
+import javax.swing.Spring;
+
+public class Livre{
+	private string numlivre;
+	private string disponible;
+	private string nom;
+	 
+public Livre(string numlivre , string disponible , string nom) {
+	 
+	this.nom=nom;
+	this.disponible=disponible;
+	this.numlivre=numlivre; 
+}
+
+}
